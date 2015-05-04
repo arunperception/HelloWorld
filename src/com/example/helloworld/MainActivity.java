42220a96b81511b402cd6570cada218286be56fc
arunperception <arun@perceptionsystem.com>
2015-05-04 17:51:36 +0530

    Comments added to class level

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+/**
+ * MainActivity Hello world sample application
+ * @author ubuntu
+ *
+ */
 public class MainActivity extends ActionBarActivity {
 
     @Override
